b1f536d0552c75c99542fc407c8983db7655363f
AryanRajdev <91838247+AryanRajdev@users.noreply.github.com>
2025-03-22 19:44:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,20 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	let element = document.querySelectorAll(".price")
+	let arr = [];
+
+	element.forEach(el => arr.push(Number(el.textContent)));
+
+	let sum =0;
+	arr.forEach(el => sum += el);
+
+	let sumRow = document.createElement("tr");
+	let sumCell = document.createElement("td");
+	sumCell.textContent = sum;
+	sumRow.appendChild(sumCell);
+
+	document.querySelector("table").appendChild(sumRow);
   
 };
 
